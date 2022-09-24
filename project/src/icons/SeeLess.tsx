68b3791631291fa698c558c83cd2c02b1d936649
murhaf Elmasri <murhaf.msri@gmail.com>
2022-09-24 22:56:47 +0300

    add icon components

---
+++ project/src/icons/SeeLess.tsx
@@ -0,0 +1,27 @@
+import { memo } from "react";
+
+export default memo(SeeLess);
+
+interface Props {
+  color?: string;
+}
+
+function SeeLess(props: Props) {
+  const color = props.color || "none";
+  return (
+    <svg
+      fill={color}
+      width="12"
+      height="1"
+      viewBox="0 0 12 1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0.00957 -8.5e-05H11.0031V0.999811H0.00957V-8.5e-05Z"
+        fill="#1D1D1D"
+      />
+    </svg>
+  );
+}
---
+++ project/src/icons/SeeMore.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+
+export default memo(SeeMore);
+
+interface Props {
+  color?: string;
+}
+
+function SeeMore(props: Props) {
+  const color = props.color || "none";
+
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M8.18423 14.2497H7.04961V8.06236H0.808395V6.93732H7.04961V0.74976H8.18423V6.93732H14.4254V8.06236H8.18423V14.2497Z"
+        fill="#1D1D1D"
+      />
+    </svg>
+  );
+}
